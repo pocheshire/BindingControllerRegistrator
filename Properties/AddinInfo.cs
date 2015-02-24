d5cc67a47168c864e7d09801bd23eb64606d9a51
Maxim Evtukh <maksevtukh@gmail.com>
2015-02-24 08:48:53 +0500

    create solution

---
+++ Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+    "BindingControllerRegistrator", 
+    Namespace = "BindingControllerRegistrator",
+    Version = "1.0"
+)]
+
+[assembly:AddinName ("BindingControllerRegistrator")]
+[assembly:AddinCategory ("BindingControllerRegistrator")]
+[assembly:AddinDescription ("BindingControllerRegistrator")]
+[assembly:AddinAuthor ("maksimevtuh")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
