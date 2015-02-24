7f9b8294c5deb28e486850df9ee5730fef1f28fc
Maxim Evtukh <maksevtukh@gmail.com>
2015-02-24 11:06:09 +0500

    добавил первоначальную логику

---
+++ Templates/BindingControllerDescriptionTemplate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MonoDevelop.Ide.Templates;
+using System.Collections.Generic;
+using System.Linq;
+using MonoDevelop.Ide;
+
+namespace BindingControllerRegistrator.Templates
+{
+    public class BindingControllerDescriptionTemplate : TextFileDescriptionTemplate
+    {
+        public override string CreateContent (MonoDevelop.Projects.Project project, Dictionary<string, string> tags, string language)
+        {
+            var temp = base.CreateContent (project, tags, language);
+
+            var projectFilesCollection = project.Files;
+            var moduleList = projectFilesCollection.Where (file => file.Name.ToLowerInvariant().Contains ("module")).ToList();
+
+            if (moduleList.Count > 1)
+            {
+
+            }
+            else
+                RegisterControllerInModule (moduleList [0], tags["FullName"]);
+
+            return temp;
+        }
+
+        private void RegisterControllerInModule (MonoDevelop.Projects.ProjectFile projectFile, string fullName)
+        {
+            var moduleDocument = IdeApp.Workbench.GetDocument (projectFile.Name);
+
+        }
+    }
+}
+
